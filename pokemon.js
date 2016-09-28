471028014713f0efd54731233aa4221a67ac2f82
David Gaynor <runnedrun@gmail.com>
2016-09-28 08:25:04 -0700

    Classadoo autocommit

--- pokemon.js
+++ pokemon.js
@@ -1,9 +1,7 @@
 function getPokemon() {
   $.ajax({
     method: "GET",
-    url: "http://pokeapi.co/api/v2/",
-    dataType: "jsonp",
-    jsonp: "callback",
+    url: "http://pokeapi.co/api/v2/",    
     success: function(res) {
       console.log("got it")
       console.log(res)
